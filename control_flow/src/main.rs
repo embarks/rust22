702bd881f343d2bf54f35e90ffe5c08144d20c2c
Emily Bartman <emily.bartman@formidable.com>
2022-05-13 16:50:21 -0700

    control_flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,5 +1,7 @@
 fn if_statement() {
-  let temp = 20;
+  let temp = 35;
+  println!("It's {} degrees outside", temp);
+
   if temp > 30 {
       println!("It's warm outside!");
   } else if temp < 10 {
@@ -7,6 +9,19 @@ fn if_statement() {
   } else {
       println!("It's a lovely day!");
   }
+
+  let day = if temp > 20 { "sunny" } else { "cloudy" };
+  println!("Today is {}", day);
+
+  println!("it is {}", if temp > 20 { "hot" } else if temp < 10 { "cold" } else {"OK"});
+  println!("it is very {}", 
+    if temp > 20 {
+      if temp > 30 {"very hot"} else {"hot"}
+    } else if temp < 10 {
+      "cold"
+    } else {
+      "OK"
+    });
 }
 
 fn main () {
